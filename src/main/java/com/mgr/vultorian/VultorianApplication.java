9a7054c4ef9a446bc51c511f1f22f04a432b89a4
Matheus <matheus.mgrbq@gmail.com>
2024-08-06 16:20:28 -0300

    first commit

---
+++ src/main/java/com/mgr/vultorian/VultorianApplication.java
@@ -0,0 +1,13 @@
+package com.mgr.vultorian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VultorianApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VultorianApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgr/vultorian/VultorianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgr.vultorian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VultorianApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
